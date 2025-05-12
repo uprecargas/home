dfa25da0befc5dc7dd2a730797fc0e193fd211e4
uprecargas <uprecargas92@gmail.com>
2025-05-12 00:04:34 -0300

    Add files via upload

---
+++ onpix/script.js
@@ -0,0 +1,17 @@
+function aplicarDesconto() {
+    const today = new Date();
+    const dayOfWeek = today.getDay(); // 0 (Domingo) a 6 (Sábado)
+
+    if (dayOfWeek === 4) { // 1 representa Segunda-feira
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            const precoElement = card.querySelector('h4');
+            let precoOriginal = parseFloat(card.dataset.preco.replace(',', '.'));
+            const desconto = precoOriginal * 0.10;
+            const precoComDesconto = precoOriginal - desconto;
+            precoElement.textContent = `R$ ${precoComDesconto.toFixed(2)}`;
+        });
+    } else {
+        alert("Hoje não temos desconto nesse produto, tente outro produto ou volte Quinta-Feira");
+    }
+}
